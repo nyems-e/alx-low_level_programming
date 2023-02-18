c0c3ed874ec6a9021e129af7de11a4844148a29a
John Doe <nyemiteijesse@gmail.com>
2023-02-18 04:19:03 -0800

    pattern

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ *main- print pattern
+ *Return:0
+ */
+
+int main(void)
+{
+int i = 1;
+int j = 0;
+int k = 0;
+int l = 0;
+while (l <= 9)
+{
+while (k <= 9)
+{
+while (j <= 9)
+{
+while (i <= 9)
+{
+putchar(l + '0');
+putchar(k + '0');
+putchar(' ');
+putchar(j + '0');
+putchar(i + '0');
+putchar(',');
+putchar(' ');
+i++;
+}
+i = 0;
+j++;
+}
+j = 0;
+k++;
+}
+k = 0;
+l++;
+}
+}
