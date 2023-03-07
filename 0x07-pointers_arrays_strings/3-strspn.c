f2884211cab98ed7c51a9d12b9aef35407c908c8
John Doe <nyemiteijesse@gmail.com>
2023-03-07 01:43:25 -0800

    1

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: intial segment 
+ * @accept: bytes to compare to
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+int acc = 0;
+int s_len = 0;
+int accept_len = 0;
+s_len = string_len(s);
+accept_len = string_len(accept);
+while(accept_len)
+{
+while (s_len)
+{
+if (s[s_len] == accept[accept_len])
+{
+acc++;
+}
+s_len--;
+}
+accept_len--;
+s_len = string_len(s);
+}
+return (acc);
+}
+
+/**
+ * string_len - length of string
+ * @s: pointer
+ * Return: length
+ */
+int string_len(char *s)
+{
+int length = 0;
+while (s[length] > '\0')
+{
+length++;
+}
+return (length);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void _putchar(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-int str_len(char *s);
+int string_len(char *s);
+unsigned int _strspn(char *S, char *accept);
 #endif 
 
