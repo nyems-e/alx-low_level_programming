64bc5599d0e5920f422e82c510fc5395639bc65b
John Doe <nyemiteijesse@gmail.com>
2023-03-08 15:44:06 -0800

    1

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef recursion
+#define recursion
+
+/*
+ * funcs
+ */
+void _putchar(int);
+void _puts_recursion(char *s);
+
+#endif
---
+++ 0x08-recursion/puthcar.c
@@ -0,0 +1,6 @@
+#include "main.h"
+#include <stdio.h>
+void _putchar(int a)
+{
+putchar(a);
+}
