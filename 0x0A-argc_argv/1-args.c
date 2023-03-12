fcb62732ebb183716ee0e7302bfff1d96a89aa96
John Doe <nyemiteijesse@gmail.com>
2023-03-12 08:12:10 -0700

    cl_arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - program prints its name and new line
+ * @argc: number of command line arguments
+ * @argv: array of arguments
+ * Return: int
+ */
+int main(int argc, __attribute__((unused))char * argv[])
+{
+_putchar(48 + argc);
+_putchar('\n');
+return (0);
+}
