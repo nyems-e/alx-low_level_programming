dabbb4e6effa2faf9e2dcedc90d4d11448e2cdda
John Doe <nyemiteijesse@gmail.com>
2023-03-12 07:57:24 -0700

    cl_arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - program prints its name and new line
+ * @argc: number of command line arguments
+ * @argv: array of arguments
+ * Return: int
+ */
+int main(int argc,char* argv[])
+{
+int i = 0;
+while(argv[0][i] > '\0')
+{
+_putchar(argv[0][i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef cl_arg
+#define cl_arg
+
+/**
+ * prototypes
+ */
+
+void _putchar(int c);
+
+#endif /* cl_arg */
