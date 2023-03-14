0c9dec2ab42f22a5d1ea596f84fc2d85b7adabf3
John Doe <nyemiteijesse@gmail.com>
2023-03-14 09:07:06 -0700

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,12 +8,54 @@
  */
 char *_strdup(char *str)
 {
-char *new_str = malloc(sizeof(str));
-new_str = str;
-if(str == NULL || new_str == NULL)
+
+if(str == NULL)
 {
 return (NULL);
 }
+	
+char *new_str = malloc(sizeof(char) * string_len(str));
+
+if(new_str == NULL)
+{
+return (NULL);
+}
+_memcpy(new_str, str, string_len(str));
 return (new_str);
 }
 
+
+/**
+ * string_len- length
+ * @s: string
+ * Return: length
+ */
+int string_len(char *s)
+{
+int length = 0;
+while (s[length] > '\0')
+{
+length++;
+}
+return (length);
+}
+
+/**
+ * _memcpy- copy memory
+ * @dest:dest pointer
+ * @src: src pointer
+ * @n: number limit
+ * Return: temp
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+char *temp;
+temp = dest;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (temp);
+}
---
+++ 0x0B-malloc_free/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+char *str = "cars";
+printf("%lu",sizeof(str));
+}
