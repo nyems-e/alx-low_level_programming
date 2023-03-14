63f83c929482ea61f04709489fee6757f9a0b82d
John Doe <nyemiteijesse@gmail.com>
2023-03-14 05:32:03 -0700

    !

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - returns a pointer 
+ *@str: original string
+ *Return: char
+ */
+char *_strdup(char *str)
+{
+char *new_str = malloc(sizeof(str));
+new_str = str;
+if(str = NULL || new_str == NULL)
+{
+return (NULL);
+}
+return (new_str);
+}
+
