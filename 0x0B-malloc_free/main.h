787abcccf0f5d3be8fe10f9da48db0396eb54df0
John Doe <nyemiteijesse@gmail.com>
2023-03-14 09:10:55 -0700

    1

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,5 +4,6 @@
 char *create_array(unsigned int size, char c);
 void _putchar(int);
 char *_strdup(char *str);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
+int string_len(char *);
 #endif
