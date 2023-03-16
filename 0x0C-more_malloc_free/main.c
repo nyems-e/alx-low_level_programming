c49c94cf25078a5e6dbba91d4e435c5c93bbccba
John Doe <nyemiteijesse@gmail.com>
2023-03-16 06:38:32 -0700

    1

---
+++ 0x0C-more_malloc_free/main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *c;
+    int *i;
+    float *f;
+    double *d;
+
+    c = malloc_checked(sizeof(char) * 1024);
+    printf("%p\n", (void *)c);
+   }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,3 +6,4 @@
  */
 void *malloc_checked(unsigned int b);
 
+#endif
