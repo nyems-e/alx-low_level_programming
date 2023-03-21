1c89b9cebc28e3de3e4791aef5da808fa41201bf
John Doe <nyemiteijesse@gmail.com>
2023-03-20 19:16:09 -0700

    1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog
+#define dog
+
+/**
+ * struct dog - element of a dog
+ * @name:name of dog
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+#endif
