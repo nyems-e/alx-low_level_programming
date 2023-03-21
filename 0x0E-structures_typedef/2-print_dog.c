2367e19b41f0d2f03ae124595ded4c6f4b4bd261
John Doe <nyemiteijesse@gmail.com>
2023-03-20 20:42:32 -0700

    1

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,5 +6,23 @@
  */
 void print_dog(struct dog *d)
 {
+if (d != NULL)
+{
+if (d->name == NULL)
+{
+printf("Name: %s\nAge: %f\nOwner: %s", "(nil)", d->age, d->owner);
+}
+else if (d->age == 0)
+{
+printf("Name: %s\nAge: %s\nOwner: %s", d->name, "(nil)", d->owner);
+}
+else if (d->owner == NULL)
+{
+printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, "(nil)");
+}
+else
+{
 printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner);
+}
+}
 }
