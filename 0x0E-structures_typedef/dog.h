5f3a7020624fc026d4d3053e9d80b73ad3cd8c84
John Doe <nyemiteijesse@gmail.com>
2023-03-21 14:31:44 -0700

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
-
+int string_len(char *s);
 #endif /* DOG_H */
