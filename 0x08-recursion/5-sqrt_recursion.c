465ff328b76c7cf405cc3e7a9a9c3ecbad69809d
Jesse <nyemiteijesse@gmail.com>
2023-06-07 07:32:14 -0700

    1

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,12 +11,16 @@ if (n < 0)
 {
 return (-1);
 }
-
-
-
+sqrt_search(n, 0 ,n);
 }
 
-
+/**
+ * sqrt_search- binary search of square root
+ * @n: squared number
+ * @start: beginning of range
+ * @end: highest number in range
+ * Return: int
+ */
 int sqrt_search(int n, int start, int end)
 {
 int mid = 0;
@@ -37,4 +41,5 @@ return sqrt_search(n, start, mid + 1);
 }
 else
 {
-
+return sqrt_search(n, mid + 1, end);
+}}
