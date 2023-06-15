9a93420ff0bc51589d8ab11268e405b07f925446
Jesse <nyemiteijesse@gmail.com>
2023-06-15 06:42:22 -0700

    1

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef m_check
+#define m_check
+
+/**
+ * prototypes
+ */
+void *malloc_checked(unsigned int b);
+int string_len(char *);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *makecpy(char *dest, char *src, unsigned int n)
+
+#endif
