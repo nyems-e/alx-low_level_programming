5f5e0ff8321c99acc3773fdbfbe55f33883e524e
Jesse <nyemiteijesse@gmail.com>
2023-06-20 03:49:52 -0700

    1

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,6 @@ if (d->name == NULL)
 {
 printf("Name: %s\nAge: %f\nOwner: %s\n", "(nil)", d->age, d->owner);
 }
-else if (d->age == 0)
-{
-printf("Name: %s\nAge: %s\nOwner: %s\n", d->name, "(nil)", d->owner);
-}
 else if (d->owner == NULL)
 {
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, "(nil)");
