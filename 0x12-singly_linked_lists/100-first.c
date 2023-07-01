b22e1bfe2428924070492fd878c9e716b6020e76
Jesse <nyemiteijesse@gmail.com>
2023-07-01 16:26:01 -0700

    extra task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_before_main- print before main
+ * Return: nothing
+ */
+
 void __attribute__((constructor)) print_before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
