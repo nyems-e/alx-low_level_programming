3f1a70664ab94d8908df6f30aa7403a78ce66c1f
Jesse <nyemiteijesse@gmail.com>
2023-07-01 16:23:44 -0700

    extra task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+#include <stdio.h>
+
+void __attribute__((constructor)) print_before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,4 +23,5 @@ int _strlen_recursion(char *s);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void __attribute__((constructor)) print_before_main();
 #endif
