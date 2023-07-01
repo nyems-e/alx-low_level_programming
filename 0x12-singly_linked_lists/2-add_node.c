a9b7159d5250ac5cdef24c80ef6dd69d339c7de5
Jesse <nyemiteijesse@gmail.com>
2023-07-01 13:43:44 -0700

    adding node at beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,4 @@
-iinclude "lists.h"
+#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
 /**
@@ -9,12 +9,12 @@ iinclude "lists.h"
  */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t temp*;
+list_t *temp;
 if (str == NULL)
 {
 	return (NULL);
 }
-*temp = malloc(sizeof(list_t));
+temp = malloc(sizeof(list_t));
 if (temp == NULL)
 {
 	return (NULL);
