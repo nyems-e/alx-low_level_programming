ac7a8812fd7c7c246aec2da92b487fe80b673f36
Jesse <nyemiteijesse@gmail.com>
2023-07-05 18:18:20 -0700

    1

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,16 +2,16 @@
 
 /**
  * free_listint2- frre list
- * @head- pointer to first node
+ * @head: pointer to first node
  */
 void free_listint2(listint_t **head)
 {
 listint_t *trck;
 while (*head == NULL)
 {
-	trck = head->next;
+	trck = (*head)->next;
 	free(*head);
 	*head = trck;
 }
-head = NULL
+head = NULL;
 }
