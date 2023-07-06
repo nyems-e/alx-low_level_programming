70c3cb7bf0438cdb56aa457f57b71379ee378adf
Jesse <nyemiteijesse@gmail.com>
2023-07-05 18:00:17 -0700

    1

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2- frre list
+ * @head- pointer to first node
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *trck;
+while (*head == NULL)
+{
+	trck = head->next;
+	free(*head);
+	*head = trck;
+}
+head = NULL
+}
