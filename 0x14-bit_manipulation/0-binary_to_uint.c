3e611b6d38f587e47d1282fce5767daaf128515b
Jesse <nyemiteijesse@gmail.com>
2023-07-07 02:42:17 -0700

    task1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#
+include "main.h"
+/**
+ * binary_to_uint- binary to unsignes integer
+ * @b: number as a string
+ * Return: converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int new_num = 0;
+if (b == NULL)i
+{
+	return (0);
+}
+while (*b != '\0')
+{
+	if (*b == '0' || *b == '1')
+	{
+		new_num = (*b - '0') + (new_num << 1);
+	        b++;	
+	}
+	else
+	{
+		return (0);
+	}
+}
+return (new_num);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef BITS_H
+#define BITS_H
+
+#include <stdlib.h>
+unsigned int binary_to_uint(const char *b);
+unsigned int size(const char *b);
+#endif
