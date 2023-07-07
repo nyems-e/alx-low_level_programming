696539c6f237756aec676c9755255d7ebdf1dd9e
Jesse <nyemiteijesse@gmail.com>
2023-07-07 07:49:17 -0700

    1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit- set value at a given index
+ * @n: number to be modified
+ * @index: index
+ * Return: 1 if it worked 0r -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= sizeof(unsigned long int) * 8)
+{
+	return (-1);
+}	
+*n = (*n | (1 << index));
+return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@ unsigned int size(const char *b);
 void _putchar(int charac);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif
