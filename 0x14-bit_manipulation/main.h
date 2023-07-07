951b2a9a6fb3b1c86461e9e827d1e529ff59e0e0
Jesse <nyemiteijesse@gmail.com>
2023-07-07 07:17:35 -0700

    1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,4 +6,5 @@ unsigned int binary_to_uint(const char *b);
 unsigned int size(const char *b);
 void _putchar(int charac);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 #endif
