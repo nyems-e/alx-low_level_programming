6eb40932f4efd92d6280485b7312aeaad68dee6a
Jesse <nyemiteijesse@gmail.com>
2023-07-11 15:45:42 -0700

    !

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,4 +14,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_file(const char *src, const char *dest);
 
-#endif`
+#endif
