64fd09dfca77dfacc1ab1b6363ffb96531328907
Jesse <nyemiteijesse@gmail.com>
2023-07-13 03:26:57 -0700

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * _strlen- length of string
+ * @str: string to find length of
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int count = 0;
+
+	while (str != NULL)
+	{
+		str++;
+		count++;
+	}
+	return (count);
+}
+
+
+/**
+ * create_file- creating a new file and reading a string into it
+ * @filename: name of file to create
+ * @text_content: string to read into file
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_des;
+	int file_des_w;
+	int text_len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		text_len = _strlen(text_content);
+	}
+	file_des = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	file_des_w = write(file_des, text_content, text_len);
+	if (file_des == -1 || file_des_w == -1)
+	{
+		return (-1);
+	}
+	close(file_des);
+	return (1);
+}
+
+
+
