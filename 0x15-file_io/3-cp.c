1f4edf8f085f0106697b7727eef51a0e354dd1e7
Jesse <nyemiteijesse@gmail.com>
2023-07-29 11:28:12 -0700

    1

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,7 +41,7 @@ void error_handle(int source_f, int des_f, char *argv[])
 	}
 	if (des_f == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
