1f136ecdf9c7462a01d43fe3ae4ca951fd0dd9db
Jesse <ebennyems@gmail.com>
2023-10-26 06:51:43 -0700

    1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,9 +13,9 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	while (*b != '\0')
 	{
-		if (*b == 1)
+		if (*b == '1')
 			conv_num = (conv_num << 1) | 1;
-		else if (*b == 0)
+		else if (*b == '0')
 			conv_num = (conv_num << 1);
 		else
 			return (0);
