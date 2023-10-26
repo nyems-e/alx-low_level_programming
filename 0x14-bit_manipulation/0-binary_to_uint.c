a329f8787d1fe61e8a41c1d01f36bfa09544adcc
Jesse <ebennyems@gmail.com>
2023-10-26 04:13:00 -0700

    question 0.0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * binary_to_uint - converts binary to integer
+ * @b: pointer to binary string
+ * Return: converted integer
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int conv_num;
+	while (*b != '\0')
+	{
+		if (b == NULL)
+			return (0);
+		else if (*b == 1)
+			conv_num = (conv_num << 1) | 1;
+		else if (*b == 0)
+			conv_num = (conv_num << 1);
+		else
+			return (0);
+		b++;
+	}
+	return (conv_num);
+}			
