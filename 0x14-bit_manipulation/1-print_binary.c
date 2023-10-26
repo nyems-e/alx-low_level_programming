3e44fe8955bc506fe4af26292291f419604e575e
Jesse <ebennyems@gmail.com>
2023-10-26 05:09:12 -0700

    question 1.1 & Readme

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * print_binary - print binary
+ * @n: integer to  be converted
+ */
+void print_binary(unsigned long int n)
+{
+	size_t bit_size;
+	size_t i;
+       	char bin_num;	
+	bit_size = sizeof(unsigned long int) * 8;
+	bit_size--;
+	while (bit_size >= 0)
+	{
+		bin_num = (n >> bit_size) & 1;
+		_putchar(bin_num + '0');
+		bit_size--;
+	}
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef BIT
+#define BIT
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
+#endif
