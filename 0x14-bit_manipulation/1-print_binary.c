7bbd4c996a3d0e0c37353f44a334336db99f9974
Jesse <ebennyems@gmail.com>
2023-10-27 04:15:08 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,20 +6,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int bit_size;
-	int bin_num = 0;
-
-	bit_size = sizeof(unsigned long int) * 8;
-	bit_size--;
-	while (bin_num == 0)
-	{
-		bin_num = (n >> bit_size) & 1;
-		bit_size--;
-	}
-	while (bit_size >= 0)
-	{
-		bin_num = (n >> bit_size) & 1;
-		_putchar(bin_num + '0');
-		bit_size--;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar('0' + (n & 1));
 }
