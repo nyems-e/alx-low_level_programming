88d92a927254ccffdacc0c29552db1994c5f5e19
Jesse <ebennyems@gmail.com>
2023-10-30 06:50:54 -0700

    !

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip bits
+ * @n: number to be flipped
+ * @m: result of flip
+ * Return: number of bits to be flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i;
+	unsigned long int b;
+
+	i = 0;
+	b = n ^ m;
+	while (b > 0)
+	{
+		i += (num_bits & 1);
+		b >>= 1;
+	}
+	return (i);
+}
