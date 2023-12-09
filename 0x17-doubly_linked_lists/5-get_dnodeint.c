87738d110c36bd2df4ca868ddeec09edfd15f084
Jesse <ebennyems@gmail.com>
2023-12-08 21:01:31 -0800

    !

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int idx = 0;
 	dlistint_t *current = head;
-	
+
 	if (head == NULL)
 		return (NULL);
 	current = head;
