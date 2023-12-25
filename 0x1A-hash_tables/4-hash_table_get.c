af0e37c047d360d85b19d1612f4b233aecb756de
Jesse <ebennyems@gmail.com>
2023-12-24 22:04:08 -0800

    hash

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -26,7 +26,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(node->key, key) == 0)
 			return (node->value);
-			node = node->next;
+		node = node->next;
 	}
 	return (NULL);
 }
